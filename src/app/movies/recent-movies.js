a0494e04d5f4133e3712650995e1c3c00c594b46
shosingh <shobhit.singh@zalando.de>
2024-07-26 19:50:54 +0200

    favfav

--- src/app/movies/recent-movies.js
+++ src/app/movies/recent-movies.js
@@ -1,6 +1,6 @@
-import { getRecentMovies } from "@/server/movies";
+import {getRecentMovies} from "@/server/movies";
 import {Suspense} from "react";
-import FallbackImg from "@/components/fallbackImg";
+import MovieCard from "@/components/movieCard";
 
 
 async function LoadedRecentMovies() {
@@ -9,19 +9,10 @@ async function LoadedRecentMovies() {
     // TODO: get carousel working.
     return (
         <div className="grid gap-4 lg:grid-cols-5">
-            {movies.map((movie) => (
-                <div key={movie.id} className="p-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
-                    <FallbackImg data={movie}/>
-                    <div className="mt-4">
-                        <h2 className="text-md font-semibold text-gray-800 dark:text-white">
-                            {movie.title}
-                        </h2>
-                        <p className="mt-2 text-gray-600 dark:text-gray-300">
-                            {movie.plot}
-                        </p>
-                    </div>
-                </div>
-            ))}
+            {movies.map((movie) => {
+                    return <MovieCard key={movie.imdbid} movie={movie}/>
+                }
+            )}
         </div>
     );
 }
@@ -29,7 +20,7 @@ async function LoadedRecentMovies() {
 export default function RecentMovies() {
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <LoadedRecentMovies />
+            <LoadedRecentMovies/>
         </Suspense>
     );
 }
--- src/server/providers/imdb/index.js
+++ src/server/providers/imdb/index.js
@@ -3,20 +3,31 @@
 import data from "./data.json";
 
 
-const transposeData=(data)=>{
-
-
-   const returnData= data.map((item)=>{
-        Object.keys(item).map(obj=>{
-             item[obj.toLowerCase()]=item[obj];
+const transposeData = (data) => {
+    const returnData = data.map((item) => {
+        Object.keys(item).map(obj => {
+            item[obj.toLowerCase()] = item[obj];
         });
         return item;
     });
 
     return returnData;
 }
-export async function getMovies() {
 
-    const tData=transposeData(data.movies);
+export async function getMovies() {
+    const tData = transposeData(data.movies);
     return tData;
+}
+
+export const toggleFavoriteMovie = (imdbID) => {
+    if (imdbID) {
+        data.movies.forEach((item) => {
+            if ((item.imdbid === imdbID || item.imdbID===imdbID )&& !item?.Favourite) {
+                item.Favourite = true;
+            } else if ((item.imdbid === imdbID || item.imdbID===imdbID) && item.Favourite===true) {
+                item.Favourite = false;
+            }
+        });
+    }
+
 }
